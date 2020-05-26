2f17bd45d8750ef351f2b6c2aefccac06ccb65b3
sunzzcc <zhongchuansun@gmail.com>
2020-05-26 13:46:12 +0800

    a

--- appveyor_windows_steps.ps1
+++ appveyor_windows_steps.ps1
@@ -2,10 +2,10 @@ $workspace=$clone_folder
 
 # $pyvers = @("36", "36-x64", "36", "36-x64", "37", "37-x64", "37", "37-x64", "37", "37-x64", "38", "38-x64", "38", "38-x64")
 $pyvers = @("36")
-ls c:\Python36\
+
 foreach ($ver in $pyvers)
 {
-    $python = c:\Python${ver}\python
+    $python = "c:\Python${ver}\python"
     ls c:\Python${ver}
     & $python -m pip install cython
     & $python -m pip install numpy
